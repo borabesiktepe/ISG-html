09967e213d03cead49e9750bd2fcec10ac5015ef
Bora Beşiktepe <borabesiktepe@gmail.com>
2023-03-28 00:52:01 +0300

    Updated Landing Page styles

---
+++ scripts/script_workplace_settings.js
@@ -0,0 +1,4 @@
+const formUpdate = document.getElementById("form-update");
+const workplaceName = document.getElementById("name");
+const workplaceDesc = document.getElementById("description");
+const workplaceId = document.getElementById("workplaceId").value;
